63c195311dd68f32565f67021ded09f46e016f95
Saqib Azim <saqib1707@users.noreply.github.com>
2016-08-03 17:52:26 +0300

    Update cat.py
    Added comments for more clarity

--- cat.py
+++ cat.py
@@ -2,19 +2,19 @@ import pygame,sys
 from pygame.locals import *
 
 pygame.init()
-fps=50
+fps=50                                           #frames per second to be displayed
 fpsTime=pygame.time.Clock()
 
-display_surf=pygame.display.set_mode((600,500))
+display_surf=pygame.display.set_mode((600,500))  # Surface Object returned which is my main  window
 
 pygame.display.set_caption('Play wid Cat')
-direction='right'
-catImg=pygame.image.load('/home/saqib1707/Desktop/My Images/cat.png')
+direction='right'                                 #direction in which cat is going to move
+catImg=pygame.image.load('/home/saqib1707/Desktop/My Images/cat.png')      #loading the cat img
 catx=150
-caty=350
+caty=350                                          # initial position of cat
 while True:
-	display_surf.fill((128,128,128))
-	if direction=='right':
+	display_surf.fill((128,128,128))          #color the background of main window
+	if direction=='right':                     # cat moves in a rectangular path
 		catx+=5
 		if catx>=450:
 			direction='up'
@@ -31,11 +31,12 @@ while True:
 		if caty>=350:
 			direction='right'
 
-	display_surf.blit(catImg,(catx,caty))
+	display_surf.blit(catImg,(catx,caty))       #blit basically copies the catimg on the main window
 
 	for event in pygame.event.get():
 		if event.type==QUIT:
 			pygame.quit()
 			sys.exit()
 		pygame.display.update()
-	fpsTime.tick(fps)
+	fpsTime.tick(fps)                        # this ensures that the speed of the game is not too fast ...not mentioning it
+	                                         # will make the game run at the speed of computer
